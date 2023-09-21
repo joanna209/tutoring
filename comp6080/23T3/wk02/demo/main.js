e196a51369dfc85c99adc44a025be53fed7e7163
joanna209 <80564580+joanna209@users.noreply.github.com>
2023-09-21 12:28:13 +1000

    feat(wk2): add solns (#2)
    * feat(wk1): add solns
    
    * feat(wk2): add solns
    
    * feat(wk2): fix typo in slides

--- comp6080/23T3/wk02/demo/main.js
+++ comp6080/23T3/wk02/demo/main.js
@@ -1,1 +1,18 @@
-console.log('hi');
+const name = document.getElementById("name");
+const locationVal = document.getElementById("location");
+const order = document.getElementById("order");
+const submitBtn = document.getElementById("submit-btn");
+
+const validate = () => {
+  if (name.value.length < 3 || name.value.length > 50) {
+    output.textContent = "Please input a valid name";
+    return false;
+  } else if (order.value.length < 3 || order.value.length > 50) {
+    output.textContent = "Please input a valid order";
+    return false;
+  }
+  output.textContent = `${name.value} has submitted an order at ${new Date().toLocaleDateString()} for ${order.value} for pickup at ${locationVal.value}`
+  return true;
+}
+
+submitBtn.addEventListener("click", validate);
