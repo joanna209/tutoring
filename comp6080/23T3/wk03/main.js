025e9d2bf1c71d2f83aee24ab660f33339bac908
joanna209 <80564580+joanna209@users.noreply.github.com>
2023-09-28 14:39:02 +1000

    feat(wk3): add solns

--- comp6080/23T3/wk03/main.js
+++ comp6080/23T3/wk03/main.js
@@ -1,5 +1,23 @@
-const element = document.createElement("h1");
-element.textContent = "Hello world";
-const element1 = document.createElement("h1");
-element1.textContent = "6080 is very epic";
-document.body.append(element, element1);
+const email = document.getElementById("name");
+const password = document.getElementById("password");
+const output = document.getElementById("output");
+const login = document.getElementById("login-button");
+
+const validate = () => {
+  if (!email.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/)) {
+    output.textContent = "Please enter a valid email";
+    return;
+  } else if (password.value.length < 3 || password.value.length > 50) {
+    output.textContent = "Please enter a valid password";
+    return;
+  }
+  const element = document.createElement("h2");
+  element.textContent = "silence";
+  document.body.append(element);
+  localStorage.setItem("token", "steven");
+  console.log(localStorage.getItem("token"));
+  // localStorage.removeItem("token");
+  return;
+}
+
+login.addEventListener("click", validate);
